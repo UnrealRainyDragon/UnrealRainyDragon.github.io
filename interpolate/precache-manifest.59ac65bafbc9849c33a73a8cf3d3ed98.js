a40b483064c020017c5749979a2d038153e7b077
Max <64359749+UnrealRainyDragon@users.noreply.github.com>
2020-04-26 14:37:35 +0200

    Added interpolate project

---
+++ interpolate/precache-manifest.59ac65bafbc9849c33a73a8cf3d3ed98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b48f7cd84a3274fab8d7b78dd4125696",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92767861567d33b6ed43",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "8c3e45f1591f779b83f0",
+    "url": "/static/css/main.4aba9c0a.chunk.css"
+  },
+  {
+    "revision": "92767861567d33b6ed43",
+    "url": "/static/js/2.3809891f.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/static/js/2.3809891f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3e45f1591f779b83f0",
+    "url": "/static/js/main.75c562e7.chunk.js"
+  },
+  {
+    "revision": "a31dd45949e5a7121ec8",
+    "url": "/static/js/runtime-main.0548c30b.js"
+  }
+]);
